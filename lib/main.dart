58fc0d3fd1280a4a0a5f629fef98f217d02e8974
kk77777 <kaushikgattani@gmail.com>
2020-04-29 22:27:25 +0530

    creating Scaffold and AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Center(child: Text('Hello')),
-    ));
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
+  ));
+}
